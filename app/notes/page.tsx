607e98a2621466d732296ef8de9bec4858c7653d
Denis Gulko <denis.gulko.1998@gmail.com>
2026-05-26 16:09:12 +0300

    fix

---
+++ app/notes/page.tsx
@@ -0,0 +1,32 @@
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+import css from "./Notes.module.css";
+import { fetchNotes } from "../../lib/api";
+import NotesClient from "./Notes.client";
+
+async function App() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["notes", 1, ""],
+    queryFn: () =>
+      fetchNotes({
+        page: 1,
+        search: undefined,
+        perPage: 12,
+      }),
+  });
+
+  return (
+    <div className={css.app}>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <NotesClient />
+      </HydrationBoundary>
+    </div>
+  );
+}
+
+export default App;
