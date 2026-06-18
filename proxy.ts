a97740dd0e064729dd97314fa5446bf4e1b30240
Denis Gulko <denis.gulko.1998@gmail.com>
2026-06-18 17:07:27 +0300

    fix

--- proxy.ts
+++ proxy.ts
@@ -3,6 +3,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { parse } from "cookie";
+import { checkSession } from "./lib/api/serverApi";
 
 const privateRoutes = ["/profile", "/notes"];
 const publicRoutes = ["/sign-in", "/sign-up"];
